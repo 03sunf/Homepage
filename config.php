5990a5c91582bd7862f3b04a52be8cd8710e7107
03sunf <contact.deepin2@gmail.com>
2019-02-08 10:31:18 +0900

    Update

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+	# Author : 03sunf
+	define(__USERNAME__	, '03sunf');
+	define(__EMAIL__		, 'contact.deepin2@gmail.com');
+	define(__FACEBOOK__	, 'https://facebook.com/profile.php?id=100005397754632&ref=bookmarks');
+	define(__TWITTER__	, 'https://twitter.com/03sunf');
+	define(__BLOG__			, 'https://03sunf.github.io');
+	define(__GITHUB__		, 'https://github.com/03sunf');
+?>
--- index.php
+++ index.php
@@ -1,28 +1,34 @@
+<?php
+	#
+	# Author : 03sunf
+	#
+	require_once 'config.php';
+?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<!-- Default Set. -->
-		<title>03sunf</title>
+		<title><?=__USERNAME__?></title>
 		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 		<link rel="stylesheet" href="/css/index.css">
 		<link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700" rel="stylesheet">
 
 		<!-- Default Metatag. -->
 		<meta charset="UTF-8">
-		<meta name="description" content="03sunf's personal homepage.">
-		<meta name="author" content="03sunf">
-		<meta name="keyword" content="03sunf">
+		<meta name="description" content="<?=__USERNAME__?>'s personal homepage.">
+		<meta name="author" content="<?=__USERNAME__?>">
+		<meta name="keyword" content="<?=__USERNAME__?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">	
 
 		<!-- Metatag for Open Graph. -->
-		<meta property="og:title" content="03sunf">
-		<meta property="og:description" content="03sunf's personal homepage.">
+		<meta property="og:title" content="<?=__USERNAME__?>">
+		<meta property="og:description" content="<?=__USERNAME__?>'s personal homepage.">
 		<meta property="og:image" content="/image/logo.png">
-		<meta property="og:url" content="http://03sunf.com/">
+		<meta property="og:url" content="http://<?=__USERNAME__?>.com/">
 
 		<!-- Metatag for Twitter. -->
-		<meta name="twitter:title" content="03sunf">
-		<meta name="twitter:description" content="03sunf's personal homepage.">
+		<meta name="twitter:title" content="<?=__USERNAME__?>">
+		<meta name="twitter:description" content="<?=__USERNAME__?>'s personal homepage.">
 		<meta name="twitter:image" content="/image/logo.png">
 		<meta name="twitter:card" content="summary_large_image">
 	</head>
@@ -31,16 +37,16 @@
 		<div class="bg"></div>
 		-->
 		<div class="center">
-			<p style="font-family: 'Amatic SC'; font-weight: 800; font-size: 72px; height: 12px;">03sunf</p>
+			<p style="font-family: 'Amatic SC'; font-weight: 800; font-size: 72px; height: 12px;"><?=__USERNAME__?></p>
 			<p style="font-family: 'Amatic SC'; font-weight: 600; font-size: 18px;">
-				<a href="mailto:contact.deepin2@gmail.com" style="color: black; text-decoration: none">(contact.deepin2@gmail.com)</a>
+				<a href="mailto:<?=__EMAIL__?>" style="color: black; text-decoration: none">(<?=__EMAIL__?>)</a>
 			</p>
 			<p style="font-family: 'Amatic SC'; font-weight: 600; font-size: 32px;">
 				<a href="/donate/" style="color: #1ddb16; text-decoration: none;">Donate</a> · 
-				<a href="https://www.facebook.com/profile.php?id=100005397754932&ref=bookmarks" style="color: #1ddb16; text-decoration: none;">Facebook</a> · 
-				<a href="https://twitter.com/03sunf" style="color: #1ddb16; text-decoration: none;">Twitter</a> · 
-				<a href="https://03sunf.github.io" style="color: #1ddb16; text-decoration: none;">Blog</a> · 
-				<a href="https://github.com/03sunf" style="color: #1ddb16; text-decoration: none;">Github</a> ·
+				<a href="<?=__FACEBOOK__?>" style="color: #1ddb16; text-decoration: none;">Facebook</a> · 
+				<a href="<?=__TWITTER__?>" style="color: #1ddb16; text-decoration: none;">Twitter</a> · 
+				<a href="<?=__BLOG__?>" style="color: #1ddb16; text-decoration: none;">Blog</a> · 
+				<a href="<?=__GITHUB__?>" style="color: #1ddb16; text-decoration: none;">Github</a> ·
 				<a href="private/" style="color: #1ddb16; text-decoration: none;">Private</a>
 			</p>
 		</div>
