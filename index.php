6c3af34335611aa85f8eb41a30c0ccce37c1e758
03sunf <contact.deepin2@gmail.com>
2019-02-08 10:03:20 +0900

    Update

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- Default Set. -->
+		<title>03sunf</title>
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+		<link rel="stylesheet" href="/css/index.css">
+		<link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700" rel="stylesheet">
+
+		<!-- Default Metatag. -->
+		<meta charset="UTF-8">
+		<meta name="description" content="03sunf's personal homepage.">
+		<meta name="author" content="03sunf">
+		<meta name="keyword" content="03sunf">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">	
+
+		<!-- Metatag for Open Graph. -->
+		<meta property="og:title" content="03sunf">
+		<meta property="og:description" content="03sunf's personal homepage.">
+		<meta property="og:image" content="/image/logo.png">
+		<meta property="og:url" content="http://03sunf.com/">
+
+		<!-- Metatag for Twitter. -->
+		<meta name="twitter:title" content="03sunf">
+		<meta name="twitter:description" content="03sunf's personal homepage.">
+		<meta name="twitter:image" content="/image/logo.png">
+		<meta name="twitter:card" content="summary_large_image">
+	</head>
+	<body>
+		<!-- Hmmm.....
+		<div class="bg"></div>
+		-->
+		<div class="center">
+			<p style="font-family: 'Amatic SC'; font-weight: 800; font-size: 72px; height: 12px;">03sunf</p>
+			<p style="font-family: 'Amatic SC'; font-weight: 600; font-size: 18px;">
+				<a href="mailto:contact.deepin2@gmail.com" style="color: black; text-decoration: none">(contact.deepin2@gmail.com)</a>
+			</p>
+			<p style="font-family: 'Amatic SC'; font-weight: 600; font-size: 32px;">
+				<a href="/donate/" style="color: #1ddb16; text-decoration: none;">Donate</a> · 
+				<a href="https://www.facebook.com/profile.php?id=100005397754932&ref=bookmarks" style="color: #1ddb16; text-decoration: none;">Facebook</a> · 
+				<a href="https://twitter.com/03sunf" style="color: #1ddb16; text-decoration: none;">Twitter</a> · 
+				<a href="https://03sunf.github.io" style="color: #1ddb16; text-decoration: none;">Blog</a> · 
+				<a href="https://github.com/03sunf" style="color: #1ddb16; text-decoration: none;">Github</a> ·
+				<a href="private/" style="color: #1ddb16; text-decoration: none;">Private</a>
+			</p>
+		</div>
+		<!-- Bottom images -->
+		<img src="/image/rick.gif" class="rick">
+		<img src="/image/morty.gif" class="morty">
+	</body>
+</html>	
